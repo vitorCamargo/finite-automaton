f35b671693fe2b16ca0c85503c2fb12b07193195
Lucas Vinicius ribeiro <lucasvribeiro14@gmail.com>
2018-11-07 23:23:27 -0200

    exemplos

--- main.py
+++ main.py
@@ -1,33 +1,27 @@
 import sys
-import turingMachine
+import finiteAutomaton
 
 def read_file():
-    config = {}
-    transition = {}
     aux = 0
-    file_name = sys.argv[1]
-    file_name = str(file_name)
+    config = []
+    transition = {}
+    
+    file_name = str(sys.argv[1])
     file = open(file_name, 'r')
-    for i in range(1, 8):
+
+    for i in range(1, 5):
         conteudo = file.readline().strip('\n').split(' ')
         config[i] = conteudo
+    
     for i in file:
         aux = aux + 1
         transition[aux] = i.strip('\n').split(' ')
     return config, transition
 
-def read_tapes():
-    tape = sys.argv[2]
-    return tape
-
 def main():
-    read_file()
-    read_tapes()
-    config = {}
-    transitions = {}
-    tape = []
     config, transitions = read_file()
-    tape = read_tapes()
-    turingMachine.machine(config, tape, transitions)
+    word = sys.argv[2]
+    
+    finiteAutomaton.machine(config, word, transitions)
 
 main()
